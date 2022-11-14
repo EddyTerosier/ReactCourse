e136e81bdda7171bd4ea3b12a710c912fa7860e4
Eddy <eddyjohn.terosier@hotmail.com>
2022-11-14 16:37:22 +0100

    animation css

--- REACTLEARNING/react-app/src/App.js
+++ REACTLEARNING/react-app/src/App.js
@@ -1,42 +1,29 @@
-import {useState} from "react"
+import { useState } from "react";
 import Item from "./Components/Item/Item";
 
 function App() {
-
   // console.log(useState(10));
 
   const [monState, setMonState] = useState(10);
   const modifyState = () => {
     setMonState(20);
-  }
+  };
   const [inputData, setInputData] = useState("Eddy le BG");
   const changeInput = (e) => {
     // console.log(e);
     setInputData(e);
-  }
-  const [toggle, setToggle] = useState(true)
+  };
+  const [toggle, setToggle] = useState(true);
   const changeState = () => {
-    setToggle(!toggle)
-  }
-  if(toggle){
-    return (
-      <div className="App">
-        <h1>Le state est true </h1>
-  
-        <button onClick={changeState}>Change state</button>
-      <Item number={monState}/>
-      </div>
-    );
-  } else if (toggle ===false){
-    return (
-      <div className="App">
-      <h1>Le state est false </h1>
-
+    setToggle(!toggle);
+  };
+  return (
+    <div className="App">
+      <div className={toggle ? "box animated" : "box"}></div>
       <button onClick={changeState}>Change state</button>
+      <Item number={monState} />
     </div>
-    )
-  }
-
+  );
 }
 
 export default App;

5399d116b0fee2547aa653cf758e0ca2d43dfb87
Eddy <eddyjohn.terosier@hotmail.com>
2022-11-14 18:32:45 +0100

    AJOUTER DES LIST VIA LE STATE

--- REACTLEARNING/react-app/src/Components/form.js
+++ REACTLEARNING/react-app/src/Components/form.js
@@ -3,7 +3,11 @@ import Item from "./Item";
 
 
 export default function Form(){
-
+    const [dataArr, setDatArr] = useState([
+        {txt: "Promenez le chien"},
+        {txt: "Coder"},
+        {txt: "Wsh"}
+    ])
 
     return (
         <div className="m-auto px-4 col-12 col-sm-10 col-lg-6">
@@ -13,10 +17,15 @@ export default function Form(){
                 <button className="mt-2 btn btn-primary d-block">Envoyez</button>
                 <h2>Liste des choses a faire : </h2>
                 <ul className="list-group">
-                    <Item/>
-                    <Item/>
-                    <Item/>
-                    <Item/>
+                    {dataArr.map((item,index) => {
+                        return (
+                            <Item
+                            txt={item.txt}
+                            key={index}
+                            id={index}
+                            />
+                        )
+                    })}
                 </ul>
             </form>
         </div>
